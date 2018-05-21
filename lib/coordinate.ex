a459ff8909ed33d84fc47084e3d77227a188bdb8
kamalogudah <mcpaul2058@gmail.com>
2018-05-21 20:25:57 +0300

    Access an agents state

--- lib/coordinate.ex
+++ lib/coordinate.ex
@@ -5,4 +5,24 @@ defmodule IslandsEngine.Coordinate do
   def start_link() do
     Agent.start_link(fn -> %Coordinate{} end)
   end
+
+  def guessed?(coordinate) do
+    Agent.get(coordinate, fn state -> state.guessed? end)
+  end
+
+  def island(coordinate) do
+    Agent.get(coordinate, fn state -> state.in_island end)
+  end
+
+  def in_island?(coordinate) do
+    case island(coordinate) do
+    :none -> false
+    _ -> true
+    end
+  end
+
+  def hit?(coordinate) do
+    in_island?(coordinate) && guessed?(coordinate)
+  end
+
 end
