e3e2ba61d1d0ee05e5f899c1c30c76966443a33e
kamalogudah <mcpaul2058@gmail.com>
2018-05-21 20:12:35 +0300

    Transform into an Agent

--- lib/coordinate.ex
+++ lib/coordinate.ex
@@ -1,4 +1,8 @@
 defmodule IslandsEngine.Coordinate do
   defstruct in_island: :none, guessed?: false
   alias IslandsEngine.Coordinate
+
+  def start_link() do
+    Agent.start_link(fn -> %Coordinate{} end)
+  end
 end
