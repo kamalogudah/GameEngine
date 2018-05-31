3a62ac49a422f2d4fe71b9b6850875f4d41a0350
kamalogudah <mcpaul2058@gmail.com>
2018-05-31 20:57:04 +0300

    Initializing Genserver State

--- lib/game.ex
+++ lib/game.ex
@@ -1,9 +1,16 @@
 defmodule IslandsEngine.Game do
+  defstruct player1: :none, player2: :none
+  alias IslandsEngine.{Game, Player}
   use GenServer
 
-  def handle_info(:first, state) do
-    IO.puts "This message has been handled by handle_info/2, matching on :first."
-    {:noreply, state}
+  def start_link(name) when not is_nil name do
+    GenServer.start_link(__MODULE__, name)
+  end
+
+  def init(name) do
+    {:ok, player1} = Player.start_link(name)
+    {:ok, player2} = Player.start_link()
+    {:ok, %Game{player1: player1, player2: player2}}
   end
 
   def handle_call(:demo, _from, state) do
@@ -13,4 +20,5 @@ defmodule IslandsEngine.Game do
   def call_demo(game) do
     GenServer.call(game, :demo)
   end
+
 end
