12acef3f4905da10e5ffcfa2990a98103a3d8da4
kamalogudah <mcpaul2058@gmail.com>
2018-05-31 20:45:28 +0300

    Introducing Calls

--- lib/game.ex
+++ lib/game.ex
@@ -5,4 +5,12 @@ defmodule IslandsEngine.Game do
     IO.puts "This message has been handled by handle_info/2, matching on :first."
     {:noreply, state}
   end
+
+  def handle_call(:demo, _from, state) do
+    {:reply, state, state}
+  end
+
+  def call_demo(game) do
+    GenServer.call(game, :demo)
+  end
 end
